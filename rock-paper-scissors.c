34aee812b3275461afc25174bfd5c6d8574e72f2
liam <liam@nehoray.net>
2024-09-08 15:30:24 +0200

    Can now play one working round of rock paper scissors

--- rock-paper-scissors.c
+++ rock-paper-scissors.c
@@ -1,34 +1,119 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int winner(int userThrow, int computerthrow);
+int playRound();
 
 int main()
 {
-    int userThrow;
-    // Ask the user to type a number
-    printf("Press 1 to throw Rock, 2 for Paper, 3 for Rock: \n");
-    scanf("%d", &userThrow);
+    srand(time(NULL)); // Initialization, should only be called once.
 
-    switch (userThrow)
-    {
-    case 1 /* Rock */:
-        printf("Rock\n");
+    int one;
+    printf("Lets play rock paper scissors! Read? \n enter any number to start: ");
+    scanf("%d", &one);
 
-        break;
-    case 2 /* Paper */:
-        printf("Paper\n");
-
-        break;
-    case 3 /* Scissors */:
-        printf("Scissors \n");
-
-        break;
-
-    default:
-        printf("You didnt press 1, 2, or 3, game lost!\n");
-        break;
-    }
+    int round = playRound();
 
     printf("\n");
     return 0;
+}
+
+int winner(int userThrow, int computerthrow)
+{
+    switch (userThrow)
+    {
+    case 1 /* Rock */:
+        switch (computerthrow)
+        {
+        case 1 /* Rock */:
+            return 2;
+            printf("I threw Rock! \n");
+            break;
+        case 2 /* paper */:
+            return 0;
+            printf("I threw Paper! ");
+            break;
+        case 3 /* Scissors */:
+            printf("I threw scissors! ");
+
+            return 1;
+            break;
+        }
+        break;
+    case 2 /* Paper */:
+        switch (computerthrow)
+        {
+        case 1 /* Rock */:
+            printf("I threw Rock! ");
+
+            return 1;
+            break;
+        case 2 /* paper */:
+            printf("I threw Paper! ");
+            return 2;
+
+            break;
+        case 3 /* Scissors */:
+            printf("I threw scissors! ");
+
+            return 0;
+
+            break;
+        }
+        break;
+
+    case 3 /* Scissors */:
+        switch (computerthrow)
+        {
+        case 1 /* Rock */:
+            printf("I threw Rock! ");
+
+            return 0;
+            break;
+        case 2 /* paper */:
+            printf("I threw Paper! ");
+
+            return 1;
+
+            break;
+        case 3 /* Scissors */:
+            printf("I threw scissors! ");
+            return 2;
+            break;
+        }
+        break;
+    }
+}
+
+int playRound()
+{
+    int userThrow;
+    int computerThrow = rand() % 4;
+    // Ask the user to type a number
+    printf("Press 1 to throw Rock, 2 for Paper, 3 for Scissors  : \n");
+    scanf("%d", &userThrow);
+    if (userThrow != 1 && userThrow != 2 && userThrow != 3)
+    {
+        printf("Please throw rock paper or scissors! \n");
+        playRound();
+    }
+    else
+    {
+        int whoWins = winner(userThrow, computerThrow);
+        switch (whoWins)
+        {
+        case 0 /* player loses */:
+            printf("You lose!! \n");
+            break;
+        case 1 /*Player wins*/:
+            printf("You win!!");
+            break;
+        case 2 /*player ties!*/:
+            printf("We tied!!");
+            break;
+        }
+        return whoWins;
+    }
 }
