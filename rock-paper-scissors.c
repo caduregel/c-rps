70e019cf7defec71da945a48db5e44ca08616307
liam <liam@nehoray.net>
2024-09-08 14:16:14 +0200

    Very basic input reading

--- rock-paper-scissors.c
+++ rock-paper-scissors.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main()
 {
-    // A comment!
-    int i;
-    for ( i = 0; i < 10; i++)
+    int userThrow;
+    // Ask the user to type a number
+    printf("Press 1 to throw Rock, 2 for Paper, 3 for Rock: \n");
+    scanf("%d", &userThrow);
+
+    switch (userThrow)
     {
-        if(i==4){
-            break;
-        }
-        printf("%d", i);
+    case 1 /* Rock */:
+        printf("Rock\n");
+
+        break;
+    case 2 /* Paper */:
+        printf("Paper\n");
+
+        break;
+    case 3 /* Scissors */:
+        printf("Scissors \n");
+
+        break;
+
+    default:
+        printf("You didnt press 1, 2, or 3, game lost!\n");
+        break;
     }
-    
+
     printf("\n");
     return 0;
 }
